f09abfca006ff7f2015a366ae41e0e1aa0d23f94
Nick Hilton <hilton@csh.rit.edu>
2014-09-01 00:52:14 +0000

    Commented my code to better reflect the new system.

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,20 @@
 var dragging = 0;
 var posX=0, posY=0;
 
-//Gets the offset of an element relative to the top left corner of the screen
+/* Adds a function to each element in the DOM to 
+   compute the offset of an element relative to 
+   the top leftcorner of the screen */
 Element.prototype.screenOffset = function () {
-  var x = this.offsetLeft - window.pageXOffset;
-  var y = this.offsetTop - window.pageYOffset;
+  //offset from the parent element
+  var x = this.offsetLeft
+  var y = this.offsetTop 
 
+  //offsets by how much the user has scrolled
+  x -= window.pageXOffset;
+  y -= window.pageYOffset;
   var element = this.offsetParent;
 
+  //calculates the offset of all parent elements and gets their offset
   while (element !== null) {
     x = parseInt (x) + parseInt (element.offsetLeft);
     y = parseInt (y) + parseInt (element.offsetTop);
@@ -15,29 +22,34 @@ Element.prototype.screenOffset = function () {
     element = element.offsetParent;
   }
 
+  //returns the x offset and y offset
   return {x:x, y:y};
 };
 
+//draws a line given starting coordinates and end coordinates
 function drawLine(startX, startY, endX, endY) {
+
+  //gets the drawing context
   var ctx = document.getElementById('foreground').getContext('2d');
 
-  //starts the mouse in the last position
+  //draws the line, and gives it a width
   ctx.beginPath();
   ctx.moveTo(startX,startY);
-
-  //draws the end of the line at the updated mouse position
   ctx.lineTo(endX,endY);
   ctx.stroke();
 }
 
 function addLayer() {
+  //the size of the foreground element
   var size = 500;
   
+  //resets the old foreground element to be a background element
   var oldFg = $('#foreground');
   oldFg.attr('id', 'background');
   oldFg.on('mousedown', canvasClick);
   oldFg.on('mousemove', canvasDrag);
 
+  //Creates and initializes a new canvas element
   var newBoard = $(document.createElement('canvas'));
   newBoard.attr('id', 'foreground');
   newBoard.attr('class', 'paintBoard');
@@ -46,17 +58,20 @@ function addLayer() {
   newBoard.on('mousedown', canvasClick);
   newBoard.on('mousemove', canvasDrag);
   
+  //adds the new canvas element to the beginning of the canvas group
   $('#canvasContainer').prepend(newBoard);
 
-
-  
+  //puts all background elements farther into the background based on position
   var currSize = size;
-
   $('#canvasContainer').children('canvas').each( function() {
     $(this).css('width', currSize+'px');
+
+    //centers the element based on its size
     var layerOffset = (size-currSize)/2;
     $(this).css('left', layerOffset+'px');
     $(this).css('top', layerOffset+'px');
+
+    //sets the size to be smaller for the next element
     currSize = currSize * 0.9;
   });
 }
@@ -72,6 +87,7 @@ function canvasClick(e) {
   posX -= offset.x;
   posY -= offset.y;
 
+  //draws a rectangle in case the user wants to just draw dots
   var ctx = $('#foreground')[0].getContext('2d');
   ctx.rect(posX,posY,1,1);
   ctx.fill();
@@ -84,6 +100,7 @@ function canvasDrag(e) {
   if(!dragging)
     return;
    
+  //stores the starting coordinates
   var startX = posX;
   var startY = posY;
  
@@ -94,14 +111,17 @@ function canvasDrag(e) {
   posX -= offset.x;
   posY -= offset.y;
     
+  //draws a line from the old coordinates to the new coordinates
   drawLine(startX, startY, posX, posY);
 }
 
-//initializes the whiteboard when the window loads
+//initializes the paint board when the window loads
 $(function() {
   
+  //adds a new layer when the addLayer button is clicked
   $('#addLayer').click(addLayer);
 
+  //The mouse has stopped dragging any time it unclicks on the canvas
   $('body').on('mouseup', function() {
     dragging = 0;
   });
