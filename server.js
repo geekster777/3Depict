afbb7d3734a1e360a5a936aef8220d0e32a86549
Nick Hilton <hilton@csh.rit.edu>
2014-09-15 02:53:01 +0000

    Changed the server to support SASS. Also converted the css to sass.

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var http = require('http');
 var express = require('express');
+var sass = require('node-sass');
+
 var app = express();
+
+app.use(sass.middleware({ 
+    src: __dirname + '/public/css/sass/',
+    dest: __dirname + '/public/css/',
+    debug: true,
+    prefix: '/css',
+    outputStyle: 'compressed'
+  }));
 
 app.use(express.static(__dirname+'/public/'));
 
-http.createServer(app).listen(82);
+var server = http.createServer(app);
+
+server.listen(82);
